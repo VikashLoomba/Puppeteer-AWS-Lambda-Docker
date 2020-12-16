d87e1b03d87e5a54e33b7fef5a792c95fc1ed486
VikashLoomba <loomba.vikash@gmail.com>
2020-12-16 13:40:18 -0800

    update

---
+++ puppeteer/Dockerfile
@@ -0,0 +1,42 @@
+# Define function directory
+ARG FUNCTION_DIR="/function"
+
+FROM node:12-buster as build-image
+# Include global arg in this stage of the build
+ARG FUNCTION_DIR
+# AWS Lambda runtime dependencies
+RUN apt-get update && \
+    apt-get install -y \
+        g++ \
+        make \
+        unzip \
+        libcurl4-openssl-dev \
+        autoconf \
+        libtool \
+        cmake
+# Copy function code
+RUN mkdir -p ${FUNCTION_DIR}/
+COPY app/ ${FUNCTION_DIR}
+RUN ls ${FUNCTION_DIR}
+ENV PUPPETEER_SKIP_CHROMIUM_DOWNLOAD=true
+WORKDIR ${FUNCTION_DIR}
+RUN npm install
+
+FROM node:12-buster-slim
+# Include global arg in this stage of the build
+ARG FUNCTION_DIR
+# Set working directory to function root directory
+WORKDIR ${FUNCTION_DIR}
+# Copy in the build image dependencies
+COPY --from=build-image ${FUNCTION_DIR} ${FUNCTION_DIR}
+RUN ls ${FUNCTION_DIR}
+# Install chromium and dependencies
+RUN apt-get update \
+    && apt-get install -y wget gnupg chromium \
+    && apt-get update \
+    && apt-get install -y fonts-ipafont-gothic fonts-wqy-zenhei fonts-thai-tlwg fonts-kacst fonts-freefont-ttf libxss1 \
+      --no-install-recommends \
+    && rm -rf /var/lib/apt/lists/*
+
+ENTRYPOINT ["/usr/local/bin/npx", "aws-lambda-ric"]
+CMD [ "app.lambdaHandler" ]
---
+++ puppeteer/app/app.js
@@ -0,0 +1,72 @@
+const puppeteer =  require("puppeteer-extra");
+// add stealth plugin and use defaults (all evasion techniques)
+const StealthPlugin = require('puppeteer-extra-plugin-stealth')
+exports.lambdaHandler = async (event) => {
+  const url = event['queryStringParameters'].url;
+  console.log(event['queryStringParameters']);
+  puppeteer.use(StealthPlugin());
+  try {
+    const browser = await puppeteer.launch({executablePath: '/usr/bin/chromium', headless: true, dumpio: true, args: ['--autoplay-policy=user-gesture-required',
+    '--disable-background-networking',
+    '--disable-background-timer-throttling',
+    '--disable-backgrounding-occluded-windows',
+    '--disable-breakpad',
+    '--disable-client-side-phishing-detection',
+    '--disable-component-update',
+    '--disable-default-apps',
+    '--disable-dev-shm-usage',
+    '--disable-domain-reliability',
+    '--disable-extensions',
+    '--disable-features=AudioServiceOutOfProcess',
+    '--disable-hang-monitor',
+    '--disable-ipc-flooding-protection',
+    '--disable-notifications',
+    '--disable-offer-store-unmasked-wallet-cards',
+    '--disable-popup-blocking',
+    '--disable-print-preview',
+    '--disable-prompt-on-repost',
+    '--disable-renderer-backgrounding',
+    '--disable-setuid-sandbox',
+    '--disable-speech-api',
+    '--disable-sync',
+    '--disk-cache-size=33554432',
+    '--hide-scrollbars',
+    '--ignore-gpu-blacklist',
+    '--metrics-recording-only',
+    '--mute-audio',
+    '--no-default-browser-check',
+    '--no-first-run',
+    '--no-pings',
+    '--no-sandbox',
+    '--no-zygote',
+    '--password-store=basic',
+    '--use-gl=swiftshader',
+    '--use-mock-keychain',
+    '--single-process']});
+    console.log("Launched");
+    const page = await browser.newPage();
+
+    if(url) {
+      await page.goto(url);
+    } else {
+      await page.goto("https://google.com");
+    }
+    
+    const pdfStream = await page.pdf();
+    await browser.close();
+    return {
+      statusCode: 200,
+      isBase64Encoded: true,
+      headers: {
+        "Content-type": "application/pdf"
+      },
+      body: pdfStream.toString("base64")
+    };
+  } catch(e) {
+    console.log(e);
+    return {
+      statusCode: 500
+    };
+  }
+    
+};
